589ea555288c0670b78e23b55e20ec579f35bb2d
ryo28ab <suzuki28ab@gmail.com>
2020-03-25 12:27:51 +0900

    [fix]startFlag

--- model/bcaster.go
+++ model/bcaster.go
@@ -40,34 +40,31 @@ func (b Bcaster) RequestBcasterLive(db *gorm.DB, nicoUserSession string) (startF
 		isLive := b.Nico.UpdateNicoStatus(db, nicoUserSession)
 		onlineCheckSlice = append(onlineCheckSlice, isLive)
 	}
-	online := false
+	isOnline := false
 	for _, onlineCheck := range onlineCheckSlice {
 		if onlineCheck {
-			online = true
+			isOnline = true
 			break
 		}
 	}
-	startFlag = b.updateStatus(db, online)
+	startFlag = b.updateStatus(db, isOnline)
 	return
 }
 
-func (b Bcaster) updateStatus(db *gorm.DB, online bool) (startFlag int) {
+func (b Bcaster) updateStatus(db *gorm.DB, isOnline bool) (startFlag int) {
 	startFlag = 0
-	status := 0
-	if online {
-		if b.Status == ONLINE {
-			startFlag = 0
-		} else {
+	status := OFFLINE
+
+	if isOnline {
+		status = ONLINE
+		if b.Status == OFFLINE && time.Since(b.UpdatedAt).Hours() > 1 {
 			startFlag = 1
 		}
-		status = ONLINE
-	} else {
-		status = OFFLINE
-		startFlag = 0
 	}
+
 	if b.Status != status || b.StartFlag != startFlag {
 		db.Model(&b).UpdateColumns(map[string]interface{}{
-			"status": status,
+			"status":     status,
 			"start_flag": startFlag,
 			"updated_at": time.Now(),
 		})
